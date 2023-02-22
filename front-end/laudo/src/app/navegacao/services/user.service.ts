868cfe7b906cf2328c76868067b5e5b68291566d
Eduardosilva3 <eduardo-silva159@hotmail.com>
2023-02-22 15:47:54 -0300

    angular

--- front-end/laudo/src/app/navegacao/services/user.service.ts
+++ front-end/laudo/src/app/navegacao/services/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
 
 
 
-  url = 'http://localhost:8080/user'; // api rest fake
+  url = 'http://172.31.15.228:8080/user'; // api rest fake
 
   // injetando o HttpClient
   constructor(private httpClient: HttpClient,  private route:Router) { }
